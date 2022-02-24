3a03528c850c1c1c2e21a26279075ac34bc5593a
Anandhi <anandhimurugan2021@gmail.com>
2022-02-24 20:51:14 +0530

    Added Palindrome

--- Reverse.py
+++ Reverse.py
@@ -12,3 +12,18 @@ print (s)
 print ("The reversed string(using recursion) is : ","")
 print (reverse(s))
 print ("Successfully reversed a string")
+
+####### .........Here Palindrome Program ........ ######
+
+def isPalindrome(s):
+    return s == s[::-1]
+ 
+ 
+# Driver code
+s = "malayalam"
+ans = isPalindrome(s)
+ 
+if ans:
+    print("Yes....It is palindrome")
+else:
+    print("No .......It is not a palindrome")
